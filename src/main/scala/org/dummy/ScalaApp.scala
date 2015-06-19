b3808a473f18afeef743628ef6e712b9dd4494a5
Alvaro Agea Herradon <alvaro.agea@gmail.com>
2015-06-20 00:47:53 +0200

    Update example

--- src/main/scala/org/dummy/ScalaApp.scala
+++ src/main/scala/org/dummy/ScalaApp.scala
@@ -1,25 +1,42 @@
 package org.dummy
 
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
 import org.apache.spark.SparkContext
-
+import org.apache.spark.sql.functions._
 
 
 object ScalaApp extends App{
-  val logFile = "person.csv"
+
+  //Suppress Spark output
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  Logger.getLogger("akka").setLevel(Level.ERROR)
+
   val sc = new SparkContext("local", "Simple", "$SPARK_HOME"
     , List("target/spark-sql-training-1.0.jar"))
   val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-  import sqlContext.createSchemaRDD
+ //Athlete,Age,Country,Game,Date,Sport,Gold,Silver,Bronze,Total
 
-  case class Person(name: String, age: Int, genre:String)
+  import org.dummy.ScalaApp.sqlContext.implicits._
+  val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true")
+    .load("OlympicAthletes.csv")
 
-  val people = sc.textFile(logFile).map(_.split(",")).map(p => Person(p(0), p(1).trim.toInt,p(2)))
+  df.registerTempTable("Records")
 
-  people.registerTempTable("people")
 
-  val teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
 
-  teenagers.map(t => "Name: " + t(0)).collect().foreach(println)
+  df.select("Country", "Total").groupBy("Country").agg(sum($"Total"), avg($"Total"))
+    .foreach(println)
+
+
+  sqlContext.sql("SELECT Country,count(1) from Records where Age > 40 group by Country")
+    .foreach(println)
+
+  sqlContext.udf.register("strLen", (s: String) => s.length())
+  sqlContext.sql("SELECT Country, strLen(Country) from Records").foreach(println)
+
+  System.in.read()
+
 
 }
