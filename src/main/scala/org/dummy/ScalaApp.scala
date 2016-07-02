7ef917d2ba72acfe8ba18632d2dedf419efd570e
Alvaro Agea Herradon <alvaro.agea@gmail.com>
2016-07-02 12:17:55 +0200

    Update spark version

--- src/main/scala/org/dummy/ScalaApp.scala
+++ src/main/scala/org/dummy/ScalaApp.scala
@@ -19,8 +19,18 @@ object ScalaApp extends App{
  //Athlete,Age,Country,Game,Date,Sport,Gold,Silver,Bronze,Total
 
   import org.dummy.ScalaApp.sqlContext.implicits._
+  
   val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true")
     .load("OlympicAthletes.csv")
+
+  val lines=sc.textFile("OlympicAthletesNoHeader.csv")
+    .map(x=>{
+      val fields=x.split(",")
+      (fields(2),(fields(9).toInt,1))
+    }).reduceByKey((acc,cur)=>(acc._1+cur._1,acc._2+cur._2))
+    .map(x=>(x._1,x._2._1/x._2._2,x._2._1)).foreach(println)
+
+  System.in.read()   
 
   df.registerTempTable("Records")
 
@@ -29,14 +39,18 @@ object ScalaApp extends App{
   df.select("Country", "Total").groupBy("Country").agg(sum($"Total"), avg($"Total"))
     .foreach(println)
 
+  System.in.read()
 
   sqlContext.sql("SELECT Country,count(1) from Records where Age > 40 group by Country")
     .foreach(println)
+
+  System.in.read()
 
   sqlContext.udf.register("strLen", (s: String) => s.length())
   sqlContext.sql("SELECT Country, strLen(Country) from Records").foreach(println)
 
   System.in.read()
+  sys.exit(0)
 
 
 }
